b8f728e68609f7c85626d9d9f9af759002ff7ef4
Ankur Kumar <ankurkumar352019@gmail.com>
2025-12-14 16:31:49 +0530

    fixed double count issue

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaEye } from "react-icons/fa";
 
 export default function VisitorCounter() {
   const [visits, setVisits] = useState(0);
+  const hasCalledApi = useRef(false);
 
   useEffect(() => {
+
+    if (hasCalledApi.current) return;
+
+    hasCalledApi.current = true;
+    
     const cookieName = "has_visited_portfolio";
     const hasVisited = getCookie(cookieName);
 
@@ -36,7 +42,7 @@ export default function VisitorCounter() {
   return (
     <div className="hidden lg:flex items-center gap-2 bg-white/10 backdrop-blur-md border border-white/20 px-3 py-1 rounded-full text-xs font-medium text-gray-200 shadow-lg hover:bg-white/20 transition-all">
       <FaEye className="text-[#00bf8f]" />
-      <span>{visits.toLocaleString()} Views</span>
+      <span>{visits.toLocaleString()} Visitors</span>
     </div>
   );
 }
